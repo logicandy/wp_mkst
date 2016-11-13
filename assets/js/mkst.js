ba8eebbf4cacea3d144973820200acb59fe6a096
Madelle Kamois <mk@mkbox.org>
2016-11-13 13:31:52 +0300

    RC1

---
+++ assets/js/mkst.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($) {
+  $('#toggle_add').accordion({
+  	active: false,
+  	collapsible: true
+  });
+  $('#toggle_track').accordion({
+    active: false,
+    collapsible: true
+  });
+  $('#ship_provider').change(function() {
+	$('#ship_desc').nextAll().remove();
+  	htmlstr = '';
+  	if (null != mkst_provider_options) {
+	  	$.each(mkst_provider_options[$('#ship_provider').val()], function(key, value) {
+	  		htmlstr = '<input type="text" name="'+key+'" placeholder="'+value+'"/>';
+	  	});
+	  	htmlstr += '<input type="submit" name="submit" class="button mini" value="'+mkst_l10n.add_button+'" />';
+	}
+  	$('#ship_desc').after(htmlstr);
+  });
+});
